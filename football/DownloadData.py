a71bf7b4caf93bc848eee729425f77d8409bbe2e
erfan akbarnezhad <87381197+erfanakk@users.noreply.github.com>
2023-10-09 12:56:42 +0330

    change src

--- football/DownloadData.py
+++ football/DownloadData.py
@@ -1,4 +1,4 @@
 from roboflow import Roboflow
-rf = Roboflow(api_key="PCJeKJYq8srNbgqs93xm")
+rf = Roboflow(api_key="your_api_code")
 project = rf.workspace("school-95f9t").project("human-detection-uerkn")
 dataset = project.version(3).download("yolov8")
